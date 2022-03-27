3b618aa5fb0a539915f3949f6f8dc24465fb3a4d
Mr Electro84 <79417900+Mr-Electro84@users.noreply.github.com>
2022-03-27 12:23:20 +0200

    Ajour d'un fenêtre (WIP)

--- SS.vb
+++ SS.vb
@@ -5,7 +5,8 @@
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         If ProgressBar1.Value = 100 Then
-            Form1.Visible = True
+            Accueil.Visible = True
+            'Form1.Visible = True
             Timer1.Enabled = False
             Me.Close()
         Else ProgressBar1.Value = ProgressBar1.Value + 5
